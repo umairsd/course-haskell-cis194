88ce2d92583204144b521ac0e4a106efc4db343f
Umair Saeed <umair.saeed@gmail.com>
2016-06-08 10:09:55 -0700

    HW5 - Solution to Exercise 5

--- hw5/Calc.hs
+++ hw5/Calc.hs
@@ -7,10 +7,32 @@ http://www.seas.upenn.edu/~cis194/spring13/hw/05-type-classes.pdf
 
 {-# OPTIONS_GHC -Wall #-}
 
+{-| Without TypeSynonymInstances we get the following error:
+Illegal instance declaration for ‘Expr StackVM.Program’
+  (All instance types must be of the form (T t1 ... tn)
+   where T is not a synonym.
+   Use TypeSynonymInstances if you want to disable this.)
+In the instance declaration for ‘Expr StackVM.Program’
+ -}
+{-# LANGUAGE TypeSynonymInstances #-}
+{- Without FlexibleInstances, we get the following compiler error
+     Illegal instance declaration for ‘Expr Program’
+      (All instance types must be of the form (T a1 ... an)
+       where a1 ... an are *distinct type variables*,
+       and each type variable appears at most once in the instance head.
+       Use FlexibleInstances if you want to disable this.)
+-}
+{-# LANGUAGE FlexibleInstances #-}
+
+
+
+
+
 module Calc where
 
 import ExprT
 import Parser
+import qualified StackVM
 
 {-|
 This is from ExprT, pasting here for reference
@@ -123,3 +145,59 @@ testSat = testExp :: Maybe Mod7
 
 
 --------------
+-- Exercise 5
+--------------
+
+instance Expr StackVM.Program where
+    lit x    = StackVM.PushI x : []
+    add x y  = x ++ y ++ [StackVM.Add]
+    mul x y  = x ++ y ++ [StackVM.Mul]
+
+compile :: String -> Maybe StackVM.Program
+compile = parseExp lit add mul
+
+
+-- Test Function
+-- stackVM takes a program and executes it
+testHelper :: Maybe StackVM.Program -> String
+testHelper Nothing     = "Not a valid program"
+testHelper (Just p)    = case StackVM.stackVM p of
+    Right x     -> show x
+    Left  err   -> err
+
+
+-- Compiles the provided string, and then tries to execute it on the
+-- VM, and prints the output (or the error)
+compileTest :: String -> String
+compileTest = testHelper . compile
+
+
+{- Example Usage of the Test function:
+t1 = compileTest "2 + 3 * 4"
+-- produces: "IVal 14"
+
+t2 = compileTest "(2 + 3) * 4"
+-- produces: "IVal 20"
+
+t3 = compileTest "(((2 * 6) + 4) * 3)"
+-- produces: "IVal 40"
+
+t4 = compileTest "(2 * 6 + 4) * 3"
+-- produces: "IVal 60"
+
+t5 = compileTest "(2 * 6 + 4) 3"
+-- produces: "Not a valid program"
+-}
+
+
+
+
+
+--------------
+-- Exercise 6
+--------------
+
+
+
+
+
