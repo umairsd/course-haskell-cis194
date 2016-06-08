f1affd6ea6a49bf5cf7c80108df6e5605755100f
Umair Saeed <umair.saeed@gmail.com>
2016-06-08 09:59:27 -0700

    HW5 - Cleanup

--- hw5/Calc.hs
+++ hw5/Calc.hs
@@ -13,6 +13,7 @@ import ExprT
 import Parser
 
 {-|
+This is from ExprT, pasting here for reference
 data ExprT = Lit Integer
            | Add ExprT ExprT
            | Mul ExprT ExprT
@@ -20,14 +21,18 @@ data ExprT = Lit Integer
 
 -}
 
+--------------
 -- Exercise 1
+--------------
 eval :: ExprT -> Integer
 eval (Lit x)      = x
 eval (Add e1 e2)  = eval e1 + eval e2
 eval (Mul e1 e2)  = eval e1 * eval e2
 
 
+--------------
 -- Exercise 2
+--------------
 evalStr :: String -> Maybe Integer
 evalStr str =
   case (parseExp Lit Add Mul str) of
@@ -35,48 +40,52 @@ evalStr str =
     Just e        -> Just (eval e)
 
 
+--------------
 -- Exercise 3
+--------------
 class Expr a where
   lit :: Integer -> a
   add :: a -> a -> a
   mul :: a -> a -> a
-
 
 instance Expr ExprT where
   lit = Lit
   add = Add
   mul = Mul
 
-
 reify :: ExprT -> ExprT
 reify = id
 
 
+--------------
 -- Exercise 4
+--------------
 
--- Integer:
--- Works like the original calculator
+-- Integer: Works like the original calculator
 instance Expr Integer where
   lit e     = e
-  add e1 e2 = e1 + e2
-  mul e1 e2 = e1 * e2
+  -- add e1 e2 = e1 + e2
+  add       = (+)
+  -- mul e1 e2 = e1 * e2
+  mul       = (*)
 
 
--- Bool
--- every literal value less than or equal to 0 is interpreted
+-- Bool:
+-- Every literal value less than or equal to 0 is interpreted
 -- as False, and all positive Integers are interpreted as True;
 -- “addition” is logical or, “multiplication” is logical and
 instance Expr Bool where
-  lit e     = e > 0
-  add b1 b2 = b1 || b2
-  mul b1 b2 = b1 && b2
+  lit e  = e > 0
+  add    = (||)
+  mul    = (&&)
 
 
+-- Defining newtypes for MinMax and Mod7, which will be used
+-- in the subsquent parts of Exercise 4
 newtype MinMax = MinMax Integer deriving (Show, Eq)
 newtype Mod7   = Mod7 Integer deriving (Show, Eq)
 
-
--- MinMax
+-- MinMax:
 -- “addition” is taken to be the max function, while
 -- “multiplication” is the min function
 instance Expr MinMax where
@@ -85,7 +94,7 @@ instance Expr MinMax where
   mul (MinMax e1) (MinMax e2) = MinMax (min e1 e2)
 
 
--- Mod7
+-- Mod7:
 -- all values should be in the ranage 0 . . . 6, and
 -- all arithmetic is done modulo 7; for example,
 -- 5 + 3 = 1.
@@ -95,7 +104,7 @@ instance Expr Mod7 where
   mul (Mod7 e1) (Mod7 e2) = lit (e1 * e2)
 
 
-
+-- Tests (from homework description)
 testExp :: Expr a => Maybe a
 testExp = parseExp lit add mul "(3 * -4) + 5"
 
@@ -112,3 +121,5 @@ testSat :: Maybe Mod7
 testSat = testExp :: Maybe Mod7
 
 
+
+--------------
