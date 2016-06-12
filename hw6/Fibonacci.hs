f4ac58179d88d77d6a1513a149408cc9ebfddabb
Umair Saeed <umair.saeed@gmail.com>
2016-06-12 13:00:44 -0700

    HW6 - Solution to Exercise 6

--- hw6/Fibonacci.hs
+++ hw6/Fibonacci.hs
@@ -6,6 +6,9 @@ http://www.seas.upenn.edu/~cs194/spring13/hw/06-laziness.pdf
 -}
 
 {-# OPTIONS_GHC -Wall #-}
+{-# OPTIONS_GHC -fno-warn-missing-methods #-}
+
+{-# LANGUAGE FlexibleInstances #-}
 
 module Fibonacci where
 
@@ -31,7 +34,7 @@ fibs2 :: [Integer]
 fibs2 = map fst $ iterate fibs2helper (0,1)
 
 fibs2helper :: (Integer, Integer) -> (Integer, Integer)
-fibs2helper (x,y) = (y, x+y)
+fibs2helper (f,s) = (s, f+s)
 
 
 --------------
@@ -42,11 +45,11 @@ data Stream a = Cons a (Stream a)
 
 
 streamToList :: Stream a -> [a]
-streamToList (Cons x xs)= x : streamToList xs
+streamToList (Cons first rest)= first : streamToList rest
 
 
 instance Show a => Show (Stream a) where
-  show strm = foldr (\x acc -> show x ++ ", " ++ acc) "" $ take 20 $ streamToList strm
+  show strm = foldr (\s acc -> show s ++ ", " ++ acc) "" $ take 20 $ streamToList strm
 
 
 
@@ -56,18 +59,18 @@ instance Show a => Show (Stream a) where
 
 -- generates a stream containing infinitely many copies of the given element
 streamRepeat :: a -> Stream a
-streamRepeat x = Cons x (streamRepeat x)
+streamRepeat e = Cons e (streamRepeat e)
 
 -- applies a function to every element of a stream
 streamMap :: (a -> b) -> Stream a -> Stream b
-streamMap f (Cons x remaining) = Cons (f x) (streamMap f remaining)
+streamMap f (Cons first rest) = Cons (f first) (streamMap f rest)
 
 -- generates a stream from "seed" of type a, which is the first element of the
 -- stream, and an "unfolding rule" of type a -> a which specifies how to
 -- transform the seed into a new seed, to be used for generating the rest of the
 -- stream
 streamFromSeed :: (a -> a) -> a -> Stream a
-streamFromSeed f x = Cons x (streamFromSeed f (f x))
+streamFromSeed f e = Cons e (streamFromSeed f (f e))
 
 {-| Example:
 >> streamFromSeed (+1) 1
@@ -99,7 +102,59 @@ powerOf2 n
 
 
 interleaveStreams :: Stream a -> Stream a -> Stream a
-interleaveStreams (Cons x xs) (Cons y ys) = Cons x (Cons y (interleaveStreams xs ys) )
+interleaveStreams (Cons z zs) (Cons y ys) = Cons z (Cons y (interleaveStreams zs ys) )
+
+
+
+--------------
+-- Exercise 6
+--------------
+
+x :: Stream Integer
+x = Cons 0 (Cons 1 (streamRepeat 0))
+
+
+
+instance Num (Stream Integer) where
+  -- fromInteger :: Integer -> a
+  fromInteger n = Cons n (streamRepeat 0)
+  -- negate :: a -> a
+  negate (Cons first rest) = Cons (-first) (negate rest)
+  --
+  (+) (Cons firstA restA) (Cons firstB restB) = Cons (firstA + firstB) (restA + restB)
+  --
+  (*) (Cons firstA restA) b@(Cons firstB restB) =
+     Cons (firstA * firstB) ((streamMap (*firstA) restB) + (restA * b))
+
+
+{-|
+
+Example usage:
+
+*Fibonacci> x^4
+0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+*Fibonacci> x^5
+0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+*Fibonacci> (1+x)^5
+1, 5, 10, 10, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+*Fibonacci> (1+x)^3
+1, 3, 3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+*Fibonacci> (1+x)^2
+1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+*Fibonacci>  (x^2 + x + 3) * (x - 5)
+-15, -2, -4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+
+-}
+
+instance Fractional (Stream Integer) where
+  (/) a@(Cons firstA restA) b@(Cons firstB restB) =
+    Cons (firstA `div` firstB) (streamMap (`div` firstB) (restA - ( a/b * restB) ))
+
+
+fibs3 :: Stream Integer
+fibs3 = x / (1 - x - x*x)
+
+
 
 
 
