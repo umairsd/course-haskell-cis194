151ec9672ee714e906a7aca1e3666d920237efcc
Umair Saeed <umair.saeed@gmail.com>
2016-06-17 15:35:27 -0700

    HW8 - Solution to Exercise 3

--- hw8/Party.hs
+++ hw8/Party.hs
@@ -27,7 +27,42 @@ instance Monoid GuestList where
 
 moreFun :: GuestList -> GuestList -> GuestList
 moreFun gl1 gl2
-    | gl1 < gl2  = gl2
+    | gl1 < gl2 = gl2
     | otherwise = gl1
+
+
+--------------
+-- Exercise 2
+--------------
+
+
+data Tree a = Node {
+     rootLabel :: a,         -- label value
+     subForest :: [Tree a]   -- zero or more child trees
+}
+
+--------------
+-- Exercise 3
+--------------
+
+-- Takes the 'boss' of the current subtree (say, Bob), and the second argument
+-- is a list of the results for each subtree under Bob. Each result is a pair
+-- of GuestsLists: the first GL is the best possible guest list with the boss
+-- of that subtree, and the second is the best possible guest list without the
+-- boss of that subtree
+nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel e@Emp {empFun = n}   []      = (GL [e] n, mempty)
+nextLevel e                    (g:gl)  = (glCons e maxGLWithoutBoss, maxGLWithBoss)
+    where
+        (maxGLWithBoss, maxGLWithoutBoss) = foldr (\x acc -> (moreFun (fst x) (fst acc), moreFun (snd x) (snd acc) )) g gl
+
+
+
+--------------
+-- Exercise 4
+--------------
+
+maxFun :: Tree Employee -> GuestList
+maxFun = undefined
 
 
